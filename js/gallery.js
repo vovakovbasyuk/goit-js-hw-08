3415c1612851d76bd5fcd889ab4713de89c37ea1
vovakovbasyuk <vova.kovbasyuk.04@gmail.com>
2024-05-27 23:04:14 +0300

    hw

--- js/gallery.js
+++ js/gallery.js
@@ -88,5 +88,49 @@ gallery.innerHTML = markup;
 
 gallery.addEventListener("click", (event) => {
   event.preventDefault();
-  if (event.target === event.currentTarget) return;
+  if (event.target.nodeName === "IMG") {
+    openModal(event.target.dataset.source);
+  }
 });
+
+document.addEventListener("keydown", (event) => {
+  const modal = document.querySelector(".modal");
+  if (
+    event.code === "Enter" ||
+    event.code === "NumpadEnter" ||
+    (event.code === "Space" && !modal)
+  ) {
+    openModal(event.target.querySelector("img").dataset.source);
+  }
+});
+
+function openModal(src) {
+  const instance = basicLightbox.create(
+    `
+    	<img src="${src}" width="1112" height="640">
+    `,
+    {
+      className: "modal",
+
+      onShow: (instance) => {
+        document.addEventListener("keydown", onEscapePress);
+      },
+
+      onClose: (instance) => {
+        document.addEventListener("keydown", onEscapePress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscapePress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+function removeFirstLaseChar(string) {
+  return string.slice(1, string.length - 1);
+}
